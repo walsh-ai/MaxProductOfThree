ae9b0fb03cfb9e5ef6432f88e51b572c06b4a2a8
csnirs <walsh.jthomas@outlook.com>
2023-09-06 16:51:48 +0100

    Full solution written, find the largest three elements in linear time and multiply for the result.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Top level statements 
+// Test the solution 
+int[] arr = {-3, 1, 2, -2, 5, 6};
+Console.WriteLine($"Test result: {MaximalProduct(arr)}");
+public static partial class Program {
+    /// <summary>
+    /// Given a non empty array of N integers A
+    /// The product of triplet P, Q, R is A[P] * A[Q] * A[R]
+    /// 0 <= P < Q < R < N
+    /// Solutions method finds the value of the maximal possible 
+    /// triplet product given array.
+    /// This solution considers that the maximal product will be 
+    /// the product of the three largest elements 
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static int MaximalProduct(int[] arr) {
+        if (arr.Length < 3)
+            throw new ArgumentException("Input array must have three or more elements"); 
+        
+        if (arr.Length == 3) 
+            return (arr[0] * arr[1] * arr[3]); 
+        
+        // Find the largest three elements 
+        int p = arr[0];
+        int q = arr[0];
+        int r = arr[0];
+
+        foreach (int n in arr) {
+            if (n > p) {
+                // Set the new largest element 
+                r = q;
+                q = p;
+                p = n; 
+            } else if (n > q) {
+                r = q;
+                q = n; 
+            } else if (n > r) {
+                r = n; 
+            }
+        }
+
+        return (p * q * r);
+    }
+}
